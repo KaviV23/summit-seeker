67e0729d51fb55a0ae061ba16928357241f48988
Ricks1104 <111761017+Ricks1104@users.noreply.github.com>
2023-08-02 10:22:23 +0800

    search.js

--- search.js
+++ search.js
@@ -4,3 +4,11 @@ document.getElementById('searchBox').addEventListener('keydown', function(e) {
         alert('You searched for: ' + this.value);
     }
 });
+
+/*const hamburgerMenu = document.querySelector('.hamburger-menu');
+const navLinks = document.getElementById('nav-links');
+
+hamburgerMenu.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+    hamburgerMenu.classList.toggle('toggle');
+});*/
